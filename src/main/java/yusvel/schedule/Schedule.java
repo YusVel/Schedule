e4bb55fc292abcbd523d01ed79ea153f0f6f2d02
YusVel <yusupvelimetov@mail.ru>
2026-02-17 19:50:41 +0300

    Начинаю настраивать главное меню

--- src/main/java/yusvel/schedule/Schedule.java
+++ src/main/java/yusvel/schedule/Schedule.java
@@ -6,6 +6,7 @@ import Table.Designations;
 import Table.MainTable;
 import Table.ScheduleTableModel;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import yusvel.schedule.employee.Employee;
 import javax.swing.*;
 import java.awt.Toolkit;
 import java.awt.FileDialog;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -28,18 +30,33 @@ import org.jdesktop.swingx.JXFrame;
 public class Schedule extends JFrame implements ActionListener, MouseListener{
     int width = Toolkit.getDefaultToolkit().getScreenSize().width/2;
     int height = Toolkit.getDefaultToolkit().getScreenSize().height/2;
-    JButton openButton = new JButton("OPEN");
+    
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menuFile = new JMenu("Файл");
+    JMenu menuOpen = new JMenu("Открыть");
+    JMenu menuHelp = new JMenu("Help");
     Schedule()
     {
-        //////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////Настройка главного окна//////////////////////////////////////////
         super("Schedule");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
         this.setBounds(this.getLocation().x-width/2+70, this.getLocation().y-height/2+40, width, height);
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        openButton.addActionListener(this);
-        this.add(openButton,BorderLayout.WEST);
+        ////////////////////////////////////////////////Настройки меню Файл/////////////////////////////////////////////////////
+        menuFile.add(new JMenu("Открыть"));
+            ((JMenu)menuFile.getMenuComponent(0)).add(new JMenuItem("График"));
+            ((JMenu)menuFile.getMenuComponent(0)).add(new JMenuItem("Список сотрудников"));
+        
+        ///////////////////////////////////////////Настройки элементов главного MENU/////////////////////////////////////////////////////
+        menuBar.add(menuFile);
+        menuBar.add(new JSeparator(JSeparator.VERTICAL));
+        menuBar.add(menuOpen);
+        menuBar.add(new JSeparator(JSeparator.VERTICAL));
+        menuBar.add(menuHelp);
+        
+        ////////////////////////////////////Добавляем все элементы в главное окно/////////////////////////////////
+        this.setJMenuBar(menuBar);
         ///////////////////////////////////////////////////////////////////////////////////////////////
         this.setVisible(true);
     }
@@ -80,12 +97,26 @@ public class Schedule extends JFrame implements ActionListener, MouseListener{
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(e.getSource() instanceof JButton b)
+        {
+            if(b.getText().equals("OPEN"))
+            {  
+              ((JButton)e.getSource()).setForeground(Color.red);
+            }
+           
+        }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(e.getSource() instanceof JButton b)
+        {
+            if(b.getText().equals("OPEN"))
+            {  
+              ((JButton)e.getSource()).setForeground(Color.BLACK);
+            }
+           
+        }
     }
   
 }

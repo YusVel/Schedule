a9a17a602d341c260802dfb28913cce52ad813b5
YusVel <yusupvelimetov@mail.ru>
2026-03-02 15:25:58 +0300

    Добавил фильтр для ввода данных в поля месяц и количество рабочих часов в окне создания таблицы

--- src/main/java/Table/CreationNewTableDialog.java
+++ src/main/java/Table/CreationNewTableDialog.java
@@ -1,5 +1,6 @@
 package Table;
 
+import Date.DatePicker;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -10,10 +11,16 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JCheckBox;
@@ -24,53 +31,74 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 import yusvel.schedule.employee.Employee;
 
-public class CreationNewTableDialog extends JDialog implements ActionListener {
+public class CreationNewTableDialog extends JDialog implements ActionListener, KeyListener {
 
     static final Font FONT = new Font("Verdena", Font.PLAIN, 14);
+
     Calendar date = Calendar.getInstance();
     Float workingHour;
     JLabel workingHourlabel = new JLabel("Количество часов \nв текущем месяце: ");
     JTextField workingHourTextField = new JTextField();
     JLabel monthLabel = new JLabel("Месяц: ");
-    JComboBox monthCombobox = new JComboBox();
+    JComboBox monthComboBox = new JComboBox(DatePicker.MONTHS_OF_YEAR);
     JLabel yearLabel = new JLabel("Год: ");
-    JComboBox yearCombobox = new JComboBox();
+    JTextField yearTextField = new JTextField(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
     JLabel employeesLabel = new JLabel("Список сотрудников");
     JLabel filters = new JLabel("Фильтры");
     JLabel postsLabel = new JLabel("по должностям:");
     JLabel departmentsLabel = new JLabel("по отделениям:");
     JPanel filtersPanel = new JPanel();
     JPanel filtersPanelPosts = new JPanel(new GridLayout(6, 2));
-    
+
     JPanel filtersPanelDepartments = new JPanel(new GridLayout(5, 1));
 
     ArrayList<JCheckBox> arrayCheckBoxesOfPosts = new ArrayList<JCheckBox>();
     ArrayList<JCheckBox> arrayCheckBoxesOfDepartments = new ArrayList<JCheckBox>();
 
     JScrollPane scrollTable;
+    JButton createButton = new JButton("Создать");
 
     Insets insets = new Insets(3, 3, 3, 3);
 
     public CreationNewTableDialog(JFrame owner, ArrayList<Employee> employees) {
         super(owner, "Создание новой таблицы");
         this.setModal(true);
-        this.setLocationRelativeTo(null);
+        this.setLocation(owner.getX() + 50, owner.getY() + 50);
         this.setLayout(new GridBagLayout());
 
         scrollTable = new JScrollPane(new CheckBoxEmployeesTable(employees));
+        scrollTable.setPreferredSize(new Dimension(500, 300));
+        createButton.addActionListener(this);
 
         workingHourlabel.setFont(FONT);
+        DocInputFilter filter = new DocInputFilter("[[0-9]+[,.]?[0-9]*]");
+        ((PlainDocument)workingHourTextField.getDocument()).setDocumentFilter(filter);
         monthLabel.setFont(FONT);
-        monthCombobox.setPreferredSize(new Dimension(80, 20));
+        monthComboBox.setPreferredSize(new Dimension(80, 20));
+        monthComboBox.addActionListener(this);
+
         yearLabel.setFont(FONT);
-        yearCombobox.setPreferredSize(new Dimension(80, 20));
+        yearTextField.setPreferredSize(new Dimension(80, 20));
+        ((PlainDocument) yearTextField.getDocument()).setDocumentFilter(new DocInputFilter("\\d*"));
+       
+        
         employeesLabel.setFont(FONT);
-        filters.setFont(new Font("Verdena", Font.BOLD, 14));
-        filtersPanel.setLayout(new GridBagLayout());
+
+        filters.setFont(
+                new Font("Verdena", Font.BOLD, 14));
+        filtersPanel.setLayout(
+                new GridBagLayout());
 
         for (int i = 0; i < Employee.POSTS.length; i++) {
             JCheckBox checbox = new JCheckBox(Employee.POSTS[i]);
@@ -85,30 +113,77 @@ public class CreationNewTableDialog extends JDialog implements ActionListener {
             arrayCheckBoxesOfDepartments.add(checbox);
         }
 
-        filtersPanel.add(filtersPanelPosts, new GridBagConstraints(0, 0, 2, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, insets, 0, 0));
-        filtersPanel.add(new JSeparator(JSeparator.VERTICAL), new GridBagConstraints(2, 0, 1, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, new Insets(3, 3, 3, 10), 0, 0));
-        filtersPanel.add(filtersPanelDepartments, new GridBagConstraints(3, 0, 1, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, insets, 0, 0));
+        filtersPanel.add(filtersPanelPosts,
+                new GridBagConstraints(0, 0, 2, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, insets, 0, 0));
+        filtersPanel.add(
+                new JSeparator(JSeparator.VERTICAL), new GridBagConstraints(2, 0, 1, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, new Insets(3, 3, 3, 10), 0, 0));
+        filtersPanel.add(filtersPanelDepartments,
+                new GridBagConstraints(3, 0, 1, 5, 0, 0, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, insets, 0, 0));
 
-        this.add(monthLabel, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(yearLabel, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(workingHourlabel, new GridBagConstraints(2, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(monthCombobox, new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(yearCombobox, new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(workingHourTextField, new GridBagConstraints(2, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(new JSeparator(JSeparator.HORIZONTAL),new GridBagConstraints(0, 3, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(filters, new GridBagConstraints(0, 4, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(postsLabel,new GridBagConstraints(0,5,2,1,1.0,1.0,GridBagConstraints.WEST,GridBagConstraints.NONE,insets,0,0));
-        this.add(departmentsLabel,new GridBagConstraints(2,5,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.NONE,insets,0,0));
-        this.add(filtersPanel, new GridBagConstraints(0, 6, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
-        this.add(scrollTable,new GridBagConstraints(0,7,3,1,1.0,1.0,GridBagConstraints.WEST,GridBagConstraints.NONE,insets,0,0));
+        this.add(monthLabel,
+                new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(yearLabel,
+                new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(workingHourlabel,
+                new GridBagConstraints(2, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(monthComboBox,
+                new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(yearTextField,
+                new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(workingHourTextField,
+                new GridBagConstraints(2, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(
+                new JSeparator(JSeparator.HORIZONTAL), new GridBagConstraints(0, 3, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(filters,
+                new GridBagConstraints(0, 4, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(postsLabel,
+                new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
+
+        this.add(departmentsLabel,
+                new GridBagConstraints(2, 5, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, insets, 0, 0));
+
+        this.add(filtersPanel,
+                new GridBagConstraints(0, 6, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, insets, 0, 0));
+
+        this.add(scrollTable,
+                new GridBagConstraints(0, 7, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.NONE, insets, 0, 0));
+
+        this.add(createButton,
+                new GridBagConstraints(0, 8, 3, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, insets, 0, 0));
+
         this.pack();
-        this.setResizable(false);
-        this.setVisible(true);
+
+        this.setResizable(
+                false);
+
+        this.setVisible(
+                true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 
 }
---
+++ src/main/java/Table/DocInputFilter.java
@@ -0,0 +1,60 @@
+package Table;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
+public class DocInputFilter extends DocumentFilter {//фильтр проверяет посимвольный ввод
+
+    String regEx;
+    int signs = 0;
+
+    public DocInputFilter(String regEx) {
+        super();
+        this.regEx = regEx;
+    }
+
+    @Override
+    public void insertString(DocumentFilter.FilterBypass fb, int offset, String string, AttributeSet attr) {
+
+        if (isNumber(string)) {
+            try {
+
+                if (string.equals(",") || string.equals(".")) {
+                    signs--;
+                }
+                super.insertString(fb, offset, string, attr);
+            } catch (BadLocationException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    @Override
+    public void replace(DocumentFilter.FilterBypass fb, int offset, int length, String text, AttributeSet attrs) {
+        if (isNumber(text) && signs < 2) {
+            System.out.println("Количество запятых: " + signs);
+            try {
+                super.replace(fb, offset, length, text, attrs);
+            } catch (BadLocationException ex) {
+                System.out.println(ex);
+            }
+        } else {
+            System.out.println("Удаляем ввод: " + text);
+            if (text.equals(",") || text.equals(".")) {
+                signs--;
+            }
+        }
+    }
+
+    public boolean isNumber(String text) {
+        if (text.equals(",") || text.equals(".")) {
+            signs++;
+        }
+        System.out.println("Введенное значение: " + text);
+        return text.matches(regEx) && signs < 2;
+    }
+
+}
